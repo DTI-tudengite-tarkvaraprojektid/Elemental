d23153b140bb92603bd4ed25482008a94b6be11a
KristerTarnamaa <krister.tarnamaa@gmail.com>
2017-05-30 11:00:07 +0300

    Lisasin lihtlabase liikumise ja hüppe funktsiooni

--- js/player.js
+++ js/player.js
@@ -5,6 +5,10 @@ function Player(game, level, x, y){
     this.y = y;
     this.sprite = null;
     this.health = 100;
+	this.facing = 'idle';
+	this.jumpTimer = 0;
+	this.jumpButton;
+	
 }
 
 Player.prototype = {
@@ -17,12 +21,59 @@ Player.prototype = {
         this.sprite.body.gravity.y = 150;
         this.sprite.body.setSize(64, 230, 40, 20);
         this.game.camera.follow(this.sprite);
-
+		this.cursors = this.game.input.keyboard.createCursorKeys()
+		this.jumpButton = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     },
 
     update: function(){
 
         this.game.physics.arcade.collide(this.sprite, this.level.wall);
+		this.sprite.body.velocity.x = 0;
+		
+		if (this.cursors.left.isDown)
+    {
+        this.sprite.body.velocity.x = -150;
+
+        if (this.facing != 'left')
+        {
+			//animatsioon siia
+            this.facing = 'left';
+        }
+    }
+    else if (this.cursors.right.isDown)
+    {
+        this.sprite.body.velocity.x = 150;
+		
+        if (this.facing != 'right')
+        {
+			//animatsioon siia
+            this.facing = 'right';
+        }
+    }
+    else
+    {
+        if (this.facing != 'idle')
+        {
+            
+            if (this.facing == 'left')
+            {
+                //player.frame = 0;
+            }
+            else
+            {
+                //player.frame = 5;
+            }
+
+            facing = 'idle';
+        }
+    }
+    
+    if (this.jumpButton.isDown && this.sprite.body.onFloor() && this.game.time.now > this.jumpTimer)
+    {
+        this.sprite.body.velocity.y = -250;
+        this.jumpTimer = this.game.time.now + 750;
+    }
+
 
 
     }
