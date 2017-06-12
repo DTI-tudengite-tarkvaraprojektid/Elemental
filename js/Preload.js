d0c49345f7e43d7a1ac295a12bf6dd12e288969a
Richard Kajaste <rkajaste@users.noreply.github.com>
2017-06-12 10:29:44 +0300

    removed peasant

--- js/Preload.js
+++ js/Preload.js
@@ -8,7 +8,6 @@ Elemental.Preload.prototype = {
         //load game assets
         this.load.tilemap('level', 'Assets/maps/level.json', null, Phaser.Tilemap.TILED_JSON);
         this.load.image('tiles', 'Assets/maps/tiles.png');
-        this.load.image('player', 'Assets/peasant.png');
         this.load.image('chest_opened', 'Assets/openchest.png');
         this.load.image('chest_closed', 'Assets/chest.png');
         this.load.spritesheet('player', 'Assets/sprite.png', 96, 128);
