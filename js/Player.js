4653ee7eadb2e498206a1d63a047c82cb1c60407
KristerTarnamaa <krister.tarnamaa@gmail.com>
2017-06-14 12:32:14 +0300

    lisatud attack animatsioonid ja väljakutsumise

--- js/Player.js
+++ js/Player.js
@@ -8,11 +8,14 @@ function Player(game, level, x, y){
 	this.jumpTimer = 0;
 	this.walking = false;
 	this.jumping = false;
+	this.attacking = false;
 	this.camera = null;
 	this.jumpAbility = true;
 	this.moveAbility = true;
 	this.chestOpen = true;
+	this.attack_cd = 0;
 	this.create();
+	
 	
 }
 
@@ -20,19 +23,21 @@ Player.prototype = {
     //create sprite here
     create: function(){
 
-        this.sprite = this.game.add.sprite(this.x, this.y, 'peasant');
+        this.sprite = this.game.add.sprite(this.x, this.y, 'player');
         this.game.physics.arcade.enable(this.sprite);
         this.sprite.body.collideWorldBounds = true;
         this.sprite.body.gravity.y = 500;
-        this.sprite.body.setSize(18, 58, 18, 5);
+        this.sprite.body.setSize(18, 64, 38, 0);
         this.camera = this.game.camera.follow(this.sprite);
-        this.sprite.animations.add('player_walk', [2, 3, 4, 5]);
-		this.sprite.animations.add('player_jump', [1]);
-        this.sprite.animations.add('idle', [0]);
+        this.sprite.animations.add('player_walk', [22, 23, 24, 25]);
+		this.sprite.animations.add('player_jump', [21]);
+        this.sprite.animations.add('idle', [20]);
+		this.sprite.animations.add('unarmored_attack', [16,17,18,19]);
         this.sprite.scale.setTo(3, 3);
         this.sprite.anchor.setTo(0.5, 0.5);
 		this.cursors = this.game.input.keyboard.createCursorKeys();
 		this.jumpButton = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		this.attackButton = this.game.input.keyboard.addKey(Phaser.Keyboard.X);
 		this.elements = [];
         this.inventory = this.game.add.group();
     },
@@ -43,7 +48,10 @@ Player.prototype = {
 		this.game.physics.arcade.overlap(this.sprite, this.level.chests, this.interact, null, this);
 		this.sprite.body.velocity.x = 0;
 		this.movement();
-
+		if (this.attackButton.isDown && !this.walking){
+			this.attack();
+			}
+		
 	},
 
     movement: function(){
@@ -68,13 +76,13 @@ Player.prototype = {
         }
 
     }
-    if(!this.walking && !this.jumping){
+    if(!this.walking && !this.jumping && !this.attacking){
 		this.sprite.animations.play('idle');
 	}
 	if(this.jumping){
 		this.sprite.animations.play('player_jump', 1, true);
 	}
-	if(!this.sprite.body.onFloor()){
+	if(!this.sprite.body.onFloor() && !this.sprite.body.blocked.up){
 		this.jumping = true;
 	} else {
 		this.jumping = false;
@@ -97,8 +105,20 @@ Player.prototype = {
 		if (this.game.input.keyboard.createCursorKeys().up.isDown && !chest.isEmpty && this.chestOpen){
 			chest.opened = true;
 		}
+	},
+	
+	attack: function (enemy){
+		if(this.game.time.now - this.attack_cd >= 450 && !this.jumping){
+            this.attack_cd = this.game.time.now;
+			this.attacking = true;
+			this.sprite.animations.play('unarmored_attack', 10, false);
+			console.log("pikachu uses quick attack");
+        }
+		if(this.sprite.body
+		this.sprite.animations.currentAnim.onComplete.add(function () {
+			this.attacking = false;
+		}, this);
 	}
-
     //all other functionalities here
 
     };
--- js/Preload.js
+++ js/Preload.js
@@ -14,6 +14,7 @@ Elemental.Preload.prototype = {
         this.load.spritesheet('enemy', 'Assets/enemy.png', 68, 64);
         this.load.spritesheet('balance', 'Assets/balance.png', 64, 64);
         this.load.spritesheet('feedback', 'Assets/feedback.png', 64, 64);
+		this.load.spritesheet('player', 'Assets/playersprites.png', 92, 64); 
         /*
         this.load.spritesheet('actions', 'Assets/actions.png');
         this.load.spritesheet('art', 'Assets/art.png');
