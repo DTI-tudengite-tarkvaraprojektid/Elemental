acc76e9657dd66f109b0e0b5a5b692f667d132e8
Richard Kajaste <rkajaste@users.noreply.github.com>
2017-06-16 13:26:43 +0300

    added timer element effect, player can die and respawn, flashing effect when hit

--- js/Game.js
+++ js/Game.js
@@ -13,15 +13,15 @@ Elemental.Game.prototype = {
     update: function(){
         this.level.update();
 
-    },/*
+    },
 
-
+    /*
     render: function (){
         this.game.debug.body(this.level.player.sprite);
         //this.level.wall.debug = true;
         this.renderGroup(this.level.enemy_objs);
-    },*/
-
+    },
+    */
     renderGroup: function(group){
         group.forEach(function (member){
             this.game.debug.body(member.sprite);
