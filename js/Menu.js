c8a1f7ee8161e088a935d28ba0c22ecabdda6296
Ege-Rita Noor <egenoor@users.noreply.github.com>
2017-06-21 01:37:16 +0300

    added player controls to menu screen

--- js/Menu.js
+++ js/Menu.js
@@ -16,6 +16,20 @@ Elemental.Menu.prototype = {
         this.button2 = this.game.add.button(SCREEN_WIDTH*0.44, SCREEN_HEIGHT*0.5, 'buttons2', this.credits, this, 0, 1, 1);
 		this.button2.scale.set(3, 3);
 		COUNTDOWN = 60;
+        SCORE = 0;
+
+        this.cursorText = this.game.add.text(SCREEN_WIDTH*0.05, SCREEN_HEIGHT*0.8, "Move",
+            {font: "24px Alagard", fill: '#d5aa00'});
+        this.cursorSprite = this.game.add.sprite(SCREEN_WIDTH*0.11, SCREEN_HEIGHT*0.77, 'cursors');
+        this.keyZText = this.game.add.text(SCREEN_WIDTH*0.05, SCREEN_HEIGHT*0.85, 'Interact',
+            {font: "24px Alagard", fill: '#d5aa00'});
+        this.keyZSprite = this.game.add.sprite(SCREEN_WIDTH*0.13, SCREEN_HEIGHT*0.85, 'keyZ');
+        this.spaceText = this.game.add.text(SCREEN_WIDTH*0.05, SCREEN_HEIGHT*0.9, 'Attack',
+            {font: "24px Alagard", fill: '#d5aa00'});
+        this.spaceSprite = this.game.add.sprite(SCREEN_WIDTH*0.11, SCREEN_HEIGHT*0.9, 'space');
+        this.cursorSprite.scale.set(1.5, 1.5);
+        this.keyZSprite.scale.set(1.5, 1.5);
+        this.spaceSprite.scale.set(1.5, 1.5);
     },
 
     startGame: function(){
